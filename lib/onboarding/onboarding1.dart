5bd86e7b2c4aae9d5d695d5f40e30f83271e9c79
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-07-15 01:09:04 +0100

    created a file called onboarding1 \ added an image called apt_logo \ created a folder for the splash screen

---
+++ lib/onboarding/onboarding1.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Onboarding1 extends StatelessWidget {
+  const Onboarding1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('Ugochukeu'),
+        ],
+      ),
+    );
+  }
+}
