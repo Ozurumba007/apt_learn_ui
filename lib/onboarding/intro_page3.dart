8aaba68ed41c0f7551c33751d2bf8c450e28f6cb
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-07-15 11:23:57 +0100

    added the logo of apt learn \ added the heading text to the screen

--- lib/onboarding/intro_page3.dart
+++ lib/onboarding/intro_page3.dart
@@ -15,9 +15,19 @@ class IntroPage3 extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Container(
+              height: 50,
+              child: Image.asset('assets/apt_learn_logo.png'),
+            ),
+            SizedBox(height: 30),
+            Container(
               height: 200,
               alignment: Alignment(0, 0.25),
               child: Image.asset('assets/improvement.png'),
+            ),
+            Text(
+              'Practice and improve your\ncoding skills',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
             ),
           ],
         ),
