e3384b87ccecef1f5e80e4ff00f482b2d900db73
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-07-15 03:03:12 +0100

    added the images to the main folder and deleted the subfolder

--- lib/onboarding/onboarding1.dart
+++ lib/onboarding/onboarding1.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class Onboarding1 extends StatelessWidget {
   const Onboarding1({super.key});
@@ -6,10 +9,21 @@ class Onboarding1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Text('Ugochukeu'),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 200,
+              child: Image.asset('assets/community.png'),
+            ),
+            SmoothPageIndicator(
+              controller: PageController(),
+              count: 3,
+            ),
+          ],
+        ),
       ),
     );
   }
